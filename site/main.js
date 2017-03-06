aa61a10293b8ae4780b842c27bee32817b7618f3
Dan Taeyoung <provolot@gmail.com>
2017-03-06 17:38:24 -0500

    presentable

--- site/main.js
+++ site/main.js
@@ -7,13 +7,13 @@ import './css/sa.sass'
 $(function() {
   $(".section").each((i, o) => {
 		console.log($(o));
-		var range = 50;
+		var range = 20;
 		$(o).css("margin-left", _.random(-range, range) + "px");
 		$(o).css("margin-right", _.random(-range, range) + "px");
 		$(o).css("margin-top", _.random(-range, range) + "px");
 		$(o).css("margin-bottom", _.random(-range, range) + "px");
     //I'm just playing, feel free to get rid of this
-    $(o).css("background-color", i%2 == 0 ? 'rgba(255,200,200,0.5)' : 'rgba(200,200,255,0.5)');
+//    $(o).css("background-color", i%2 == 0 ? 'rgba(255,200,200,0.5)' : 'rgba(200,200,255,0.5)');
 	});
 });
 
